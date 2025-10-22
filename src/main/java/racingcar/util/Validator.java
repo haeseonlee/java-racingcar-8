a77af3d66b346d086483b930ff988bd0de7ba258
haeseon lee <haeseon4816@gmail.com>
2025-10-22 14:23:10 +0900

    refactor: 입력값 공백 검증 메서드 접근 제어자 변경

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -2,7 +2,7 @@ package racingcar.util;
 
 public class Validator {
 
-    private static void validateEmptyInput(String input) {
+    public static void validateEmptyInput(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
         }
