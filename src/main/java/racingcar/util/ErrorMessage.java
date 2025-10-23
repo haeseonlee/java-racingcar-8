51060cf481bcaf822a9f964eb33ca9673cfe4437
haeseon lee <haeseon4816@gmail.com>
2025-10-23 22:38:10 +0900

    feat: 1보다 작은 시도 횟수 예외 메서드 구현

--- src/main/java/racingcar/util/ErrorMessage.java
+++ src/main/java/racingcar/util/ErrorMessage.java
@@ -3,7 +3,8 @@ package racingcar.util;
 public enum ErrorMessage {
     EMPTY_INPUT("입력값이 비어있습니다."),
     MORE_THAN_FIVE_CHAR("자동차 이름은 5글자 이하로 제한됩니다."),
-    NOT_NUMERIC_INPUT("숫자 이외의 값은 입력할 수 없습니다.");
+    NOT_NUMERIC_INPUT("숫자 이외의 값은 입력할 수 없습니다."),
+    TRY_NUMBER_TOO_SMALL("시도 횟수는 1번 이상이어야 합니다.");
 
 
     private final String message;
--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -2,9 +2,17 @@ package racingcar.util;
 
 public class Validator {
 
+    private static final int MIN_TRY_NUMBER = 1;
+
     public static void validateEmptyInput(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
+        }
+    }
+
+    public static void validateMoreThanOne(int tryNumber) {
+        if (tryNumber < MIN_TRY_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.TRY_NUMBER_TOO_SMALL.getMessage());
         }
     }
 }
