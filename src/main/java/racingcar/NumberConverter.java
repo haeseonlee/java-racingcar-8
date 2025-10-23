6cb8086a65f97d06427cbdcd4dd6f9f5114eb2f6
haeseon lee <haeseon4816@gmail.com>
2025-10-23 22:29:45 +0900

    feat: 시도 횟수 공백 입력 검증 구현

--- src/main/java/racingcar/NumberConverter.java
+++ src/main/java/racingcar/NumberConverter.java
@@ -1,14 +1,20 @@
 package racingcar;
 
 import racingcar.util.ErrorMessage;
+import racingcar.util.Validator;
 
 public class NumberConverter {
 
     public static int convert(String input) {
         try {
+            requireTryNumberInput(input);
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.NOT_NUMERIC_INPUT.getMessage());
         }
+    }
+
+    private static void requireTryNumberInput(String input) {
+        Validator.validateEmptyInput(input);
     }
 }
