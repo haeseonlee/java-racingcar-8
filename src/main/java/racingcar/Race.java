8be5e152449099e43aeb40d27a2d9c43098a3eb8
haeseon lee <haeseon4816@gmail.com>
2025-10-24 15:19:48 +0900

    feat: 경주할 자동차 불러오기 구현

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class Race {
+
+    private final Cars cars;
+
+    public Race(Cars cars) {
+        this.cars = cars;
+    }
+
+    public Cars getCars() {
+        return cars;
+    }
+}
