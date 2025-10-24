6a10539288929805111887815773e8c2432cd1eb
haeseon lee <haeseon4816@gmail.com>
2025-10-24 16:32:27 +0900

    feat: 우승한 자동차 이름 저장하는 메서드 구현

--- src/main/java/racingcar/Winner.java
+++ src/main/java/racingcar/Winner.java
@@ -1,6 +1,17 @@
 package racingcar;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Winner {
+
+    public List<String> getWinners(Cars cars) {
+        int maxPosition = getMaxPosition(cars);
+        return cars.getCars().stream()
+                .filter(car -> maxPosition == car.getPosition())
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
 
     private int getMaxPosition(Cars cars) {
         int maxPosition = 0;
