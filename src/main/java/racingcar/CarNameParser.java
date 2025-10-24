30e68ecc24b7101febad8615c99d437eec118158
haeseon lee <haeseon4816@gmail.com>
2025-10-24 11:28:44 +0900

    feat: 구분자로 끝나는 입력값 검증 로직 적용

--- src/main/java/racingcar/CarNameParser.java
+++ src/main/java/racingcar/CarNameParser.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import racingcar.util.Validator;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,7 +10,12 @@ public class CarNameParser {
     private static final String COMMA_DELIMITER = ",";
 
     public static List<String> parse(String input) {
+        validateNotEndWithDelimiter(input);
         return Arrays.stream(input.split(COMMA_DELIMITER))
                 .toList();
+    }
+
+    private static void validateNotEndWithDelimiter(String input) {
+        Validator.validateEndWithComma(input);
     }
 }
