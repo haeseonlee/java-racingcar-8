9fc1f51d6178635eef70d7c0f839c67f47aa2993
haeseon lee <haeseon4816@gmail.com>
2025-10-24 10:45:14 +0900

    feat: 구분자로 끝나는 입력값 검증 메서드 구현

--- src/main/java/racingcar/util/ErrorMessage.java
+++ src/main/java/racingcar/util/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     MORE_THAN_FIVE_CHAR("자동차 이름은 5글자 이하로 제한됩니다."),
     NOT_NUMERIC_INPUT("숫자 이외의 값은 입력할 수 없습니다."),
     TRY_NUMBER_TOO_SMALL("시도 횟수는 1번 이상이어야 합니다."),
-    NOT_STRING_INPUT("자동차 이름은 문자만 입력 가능합니다.");
+    NOT_STRING_INPUT("자동차 이름은 문자만 입력 가능합니다."),
+    END_WITH_COMMA("자동차 이름을 끝까지 입력해 주세요.");
 
 
     private final String message;
--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -3,6 +3,7 @@ package racingcar.util;
 public class Validator {
 
     private static final int MIN_TRY_NUMBER = 1;
+    private static final String COMMA_DELIMITER = ",";
 
     public static void validateEmptyInput(String input) {
         if (input == null || input.isEmpty()) {
@@ -19,6 +20,12 @@ public class Validator {
     public static void validateStringInput(String input) {
         if (input.matches("\\d+")) {
             throw new IllegalArgumentException(ErrorMessage.NOT_STRING_INPUT.getMessage());
+        }
+    }
+
+    public static void validateEndWithComma(String input) {
+        if (input.endsWith(COMMA_DELIMITER)) {
+            throw new IllegalArgumentException(ErrorMessage.END_WITH_COMMA.getMessage());
         }
     }
 }
